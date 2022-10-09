860b7b9a2d0638143508c4ec458765a0940e7059
nicodax <he201753@stutends.ephec.be>
2022-10-09 23:25:21 +0200

    feat: Created Web Django Views
    created a view for each template

--- web/views.py
+++ web/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def auth(request):
+    return render(request, 'web/auth.html', {})
+
+def dashboard(request):
+    return render(request, 'web/dashboard.html', {})
+
+def pointcloud(request):
+    return render(request, 'web/pointcloud.html', {})
