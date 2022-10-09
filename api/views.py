5258e9444bd2d3d3508ea156d732883002379e9a
nicodax <he201753@stutends.ephec.be>
2022-10-09 22:55:39 +0200

    feat: Created Api Views
    created two views in the api application:
    * getRoutes	-	returning an array of the routes available
                		publicly.
    			!#! NOTE !#!
    			this solution requires me to update the response
      			each time a new route is created
    * helloSpot	-	returning "Hello, Spot!"
    
    added rest_framework to the list of installed apps

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/hello-spot/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Hello, Spot!'
+        },
+    ]
+    return JsonResponse(routes, safe=False)
+
+def helloSpot(request):
+    return HttpResponse('Hello, Spot!')
--- spotUtils/settings.py
+++ spotUtils/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'web',
     'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
