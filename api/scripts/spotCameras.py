2e1d9a4ba5c43675192151b1560a73912a9aef48
nicodax <he201753@stutends.ephec.be>
2022-11-24 10:03:23 +0100

    feat: WebCam class constructor

---
+++ api/scripts/spotCameras.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2022 Boston Dynamics, Inc.  All rights reserved.
+#
+# Downloading, reproducing, distributing or otherwise using the SDK Software
+# is subject to the terms and conditions of the Boston Dynamics Software
+# Development Kit License (20191101-BDSDK-SL).
+
+# See https://github.com/boston-dynamics/spot-sdk/blob/master/python/examples/web_cam_image_service/web_cam_image_service.py for the original file
+
+#!/usr/bin/env python
+"""Services for fetching live video feed from spot's cameras"""
+
+# Imports
+import logging
+import cv2
+
+# Boston Dynamics
+from bosdyn.client.image_service_helpers import CameraInterface
+
+# Local Imports
+
+# Variable
+_LOGGER = logging.getLogger(__name__)
+
+# Main
+class WebCam(CameraInterface):
+    """Provide access to the latest web cam data using openCV's VideoCapture."""
+    def __init__(self, device_name):
+        """
+        Construct a new WebCam instance
+
+            Parameters:
+                device_name (str): camera device name (integer-convertible)
+        """
+        device_name = int(device_name)
+        self.capture = cv2.VideoCapture(device_name)
+        if not self.capture.isOpened():
+            err = "Unable to open a cv2.VideoCapture connection to %s" % device_name
+            _LOGGER.warning(err)
+            raise Exception(err)
+    
+    def blocking_capture(self):
+        pass
+    
+    def image_decode(self, image_data, image_proto, image_req):
+        pass
