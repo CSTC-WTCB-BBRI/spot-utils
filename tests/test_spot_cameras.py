44758bc49dfd76028655cb69438ffbfbd6a1c3aa
nicodax <he201753@stutends.ephec.be>
2022-11-24 16:16:00 +0100

    test: Created test for make_webcam_image_service

--- tests/test_spot_cameras.py
+++ tests/test_spot_cameras.py
@@ -12,6 +12,7 @@ from PIL import Image
 from django.test import TestCase
 
 ## Boston Dynamics
+import bosdyn.client.util
 from bosdyn.api import image_pb2
 from bosdyn.client.image import build_image_request
 
@@ -24,10 +25,13 @@ class WebCamTestCase(TestCase):
     Test cases for the WebCam Camera Interface.
     """
     # Constants
-    mocked_camera_gain = 10
-    mocked_camera_exposure = 150
-    mocked_camera_rows = 200
-    mocked_camera_cols = 100
+    MOCKED_CAMERA_GAIN = 10
+    MOCKED_CAMERA_EXPOSURE = 150
+    MOCKED_CAMERA_ROWS = 200
+    MOCKED_CAMERA_COLS = 100
+    SERVICE_NAME = 'SpotCameras'
+    CLIENT_NAME = 'Test Spot'
+    ROBOT_IP="0.0.0.0"
 
     def setUp(self):
         """
@@ -53,13 +57,13 @@ class WebCamTestCase(TestCase):
     def mocked_capture_get(self, propId):
         match propId:
             case cv2.CAP_PROP_GAIN:
-                return WebCamTestCase.mocked_camera_gain
+                return WebCamTestCase.MOCKED_CAMERA_GAIN
             case cv2.CAP_PROP_EXPOSURE:
-                return WebCamTestCase.mocked_camera_exposure
+                return WebCamTestCase.MOCKED_CAMERA_EXPOSURE
             case cv2.CAP_PROP_FRAME_HEIGHT:
-                return WebCamTestCase.mocked_camera_rows
+                return WebCamTestCase.MOCKED_CAMERA_ROWS
             case cv2.CAP_PROP_FRAME_WIDTH:
-                return WebCamTestCase.mocked_camera_cols
+                return WebCamTestCase.MOCKED_CAMERA_COLS
             case other:
                 return 0
 
@@ -71,10 +75,10 @@ class WebCamTestCase(TestCase):
         """
         self.device_name = '0'
         webCam = WebCam(self.device_name)
-        self.assertEqual(webCam.camera_gain, self.mocked_camera_gain)
-        self.assertEqual(webCam.camera_exposure, self.mocked_camera_exposure)
-        self.assertEqual(webCam.rows, self.mocked_camera_rows)
-        self.assertEqual(webCam.cols, self.mocked_camera_cols)
+        self.assertEqual(webCam.camera_gain, self.MOCKED_CAMERA_GAIN)
+        self.assertEqual(webCam.camera_exposure, self.MOCKED_CAMERA_EXPOSURE)
+        self.assertEqual(webCam.rows, self.MOCKED_CAMERA_ROWS)
+        self.assertEqual(webCam.cols, self.MOCKED_CAMERA_COLS)
     
     def mocked_capture_read_unsuccesful(self):
         image = Image.open("tests/src/test.jpeg")
@@ -113,3 +117,26 @@ class WebCamTestCase(TestCase):
     #     image_proto = image_pb2.Image()
     #     image_req = build_image_request('test', webCam.default_jpeg_quality, image_pb2.Image.FORMAT_JPEG, image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U8, None)
     #     webCam.image_decode(image, image_proto, image_req)
+
+    def mocked_camera_base_image_servicer_constructor(self, bosdyn_sdk_robot, service_name, image_sources, logger):
+        self.image_sources_mapped = dict()
+        self.service_name = service_name
+        self.image_sources = image_sources
+
+    @mock.patch('cv2.VideoCapture.get', mocked_capture_get)
+    @mock.patch('bosdyn.client.image_service_helpers.CameraBaseImageServicer.__init__', mocked_camera_base_image_servicer_constructor)
+    def test_service_servicer_creation(self):
+        """
+        Test case for checking the service servicer creation.
+        """
+        self.device_name = '0'
+        device_names = [self.device_name]
+        sdk = bosdyn.client.create_standard_sdk(self.CLIENT_NAME)
+        robot = sdk.create_robot(self.ROBOT_IP)
+        service_servicer = WebCam.make_webcam_image_service(robot, self.SERVICE_NAME, device_names)
+        deviceIdxO_params = service_servicer.image_sources[0].get_image_capture_params()
+        self.assertEqual(service_servicer.service_name, self.SERVICE_NAME)
+        self.assertEqual(service_servicer.image_sources[0].image_source_proto.rows, self.MOCKED_CAMERA_ROWS)
+        self.assertEqual(service_servicer.image_sources[0].image_source_proto.cols, self.MOCKED_CAMERA_COLS)
+        self.assertEqual(deviceIdxO_params.gain, self.MOCKED_CAMERA_GAIN)
+        self.assertEqual(deviceIdxO_params.exposure_duration.seconds, self.MOCKED_CAMERA_EXPOSURE)
