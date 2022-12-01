e1152b007fb1cdb70d29bc02a51d70ebe2a128bf
nicodax <he201753@stutends.ephec.be>
2022-12-01 18:12:54 +0100

    feat: getting spot cam's feed in browser

--- api/scripts/capture_laptop_frontcam_feed.py
+++ api/scripts/capture_laptop_frontcam_feed.py
@@ -2,15 +2,76 @@ import numpy as np
 import cv2 as cv
 import threading
 
+#########################
+
+# TO DO
+# REMOVE USELESS IMPORTS
+
+#########################
+
+import argparse
+import sys
+import os
+
+import cv2
+import numpy as np
+from scipy import ndimage
+
+import bosdyn.client
+import bosdyn.client.util
+from bosdyn.api import image_pb2
+from bosdyn.client.image import ImageClient, build_image_request
+
+## Environment variables
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+GUID = os.getenv('GUID')
+SECRET = os.getenv('SECRET')
+ROBOT_IP = os.getenv('ROBOT_IP')
+
 class CaptureLaptop(object):
 
-    def __init__(self, camera_index):
-        self.video = cv.VideoCapture(camera_index)
-        (self.grabbed, self.frame) = self.video.read()
+    def __init__(self, camera_index, image_client):
+        self.frame = None
+        self.image_client = image_client
+        self.getImage()
+        self.updating = True
         threading.Thread(target=self.update, args=()).start()
     
     def __del__(self):
-        self.video.release()
+        self.updating = False
+    
+    def getImage(self):
+        image_responses = self.image_client.get_image_from_sources(['frontleft_fisheye_image'])
+        image = image_responses[0]
+        num_bytes = 1  # Assume a default of 1 byte encodings.
+        if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+            dtype = np.uint16
+            extension = ".png"
+        else:
+            if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGB_U8:
+                num_bytes = 3
+            elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGBA_U8:
+                num_bytes = 4
+            elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U8:
+                num_bytes = 1
+            elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U16:
+                num_bytes = 2
+            dtype = np.uint8
+            extension = ".jpg"
+
+        img = np.frombuffer(image.shot.image.data, dtype=dtype)
+        if image.shot.image.format == image_pb2.Image.FORMAT_RAW:
+            try:
+                img = img.reshape((image.shot.image.rows, image.shot.image.cols, num_bytes))
+            except ValueError:
+                img = cv2.imdecode(img, -1)
+        else:
+            img = cv2.imdecode(img, -1)
+        self.frame = img
+
 
     def get_frame(self):
         frame = self.frame
@@ -21,8 +82,8 @@ class CaptureLaptop(object):
             return False
     
     def update(self):
-        while True:
-            (self.grabbed, self.frame) = self.video.read()
+        while self.updating:
+            self.getImage()
 
 def gen(camera):
     while True:
--- api/views.py
+++ api/views.py
@@ -3,16 +3,31 @@
 
 
 # Imports
+import os
+
 ## Django REST framework
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import StreamingHttpResponse
 from django.views.decorators import gzip
 
+## Bosdyn
+import bosdyn.client
+from bosdyn.client.image import ImageClient
+
 # Local imports
 from .scripts.helloSpot import main as helloSpot
 from .scripts.capture_laptop_frontcam_feed import gen, CaptureLaptop
 
+## Environment variables
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+ROBOT_IP = os.getenv('ROBOT_IP')
+GUID = os.getenv('GUID')
+SECRET = os.getenv('SECRET')
+
+# Variables
 camera = None
 
 # Main
@@ -52,8 +67,15 @@ def getCameraFeed(request):
     API endpoint for the camera live video feed
     """
     global camera
+    # Create robot object with an image client.
+    sdk = bosdyn.client.create_standard_sdk('image_capture')
+    robot = sdk.create_robot(ROBOT_IP)
+    robot.authenticate_from_payload_credentials(GUID, SECRET)
+    robot.sync_with_directory()
+    robot.time_sync.wait_for_sync()
+    image_client = robot.ensure_client(ImageClient.default_service_name)
     try : 
-        camera = CaptureLaptop(0)
+        camera = CaptureLaptop(0, image_client)
         return StreamingHttpResponse(gen(camera), content_type="multipart/x-mixed-replace;boundary=frame")
     except:
         pass
