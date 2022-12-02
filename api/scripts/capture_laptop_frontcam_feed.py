a95ca19041b3fc8a3e46298cb0bf7f6a91877c33
nicodax <he201753@stutends.ephec.be>
2022-12-02 11:15:17 +0100

    wip

--- api/scripts/capture_laptop_frontcam_feed.py
+++ api/scripts/capture_laptop_frontcam_feed.py
@@ -1,9 +1,18 @@
 import numpy as np
+from scipy import ndimage
 import cv2 as cv
 import threading
 
 import bosdyn.client
 from bosdyn.api import image_pb2
+
+ROTATION_ANGLE = {
+    'back_fisheye_image': 0,
+    'frontleft_fisheye_image': -90,
+    'frontright_fisheye_image': -90,
+    'left_fisheye_image': 0,
+    'right_fisheye_image': 180
+}
 
 class CaptureLaptop(object):
 
@@ -35,7 +44,6 @@ class CaptureLaptop(object):
                 num_bytes = 2
             dtype = np.uint8
             extension = ".jpg"
-
         img = np.frombuffer(image.shot.image.data, dtype=dtype)
         if image.shot.image.format == image_pb2.Image.FORMAT_RAW:
             try:
@@ -44,6 +52,7 @@ class CaptureLaptop(object):
                 img = cv.imdecode(img, -1)
         else:
             img = cv.imdecode(img, -1)
+        img = ndimage.rotate(img, ROTATION_ANGLE[image.source.name])
         self.frame = img
 
 
