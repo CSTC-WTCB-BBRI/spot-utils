06b390f24b7302ffdebf10632cbb333d030c61d2
nicodax <he201753@stutends.ephec.be>
2022-12-23 18:29:06 +0100

    * feat: Open RICOH THETA Z1 from browser
    start gst_loopback
    start SpotCameras image service
    
    also centered dashboard page elements
    
    * feat: Automatically closing SpotCameras Image Service when no longer
    needed

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@
 from django.urls import path
 
 # Local imports
-from .views import ApiRoutes, HelloSpot, getCameraFeed, closeCameraFeed, startGstLoopbackView, stopGstLoopbackView, startSpotCamerasImageServiceView, stopSpotCamerasImageServiceView
+from .views import ApiRoutes, HelloSpot, getCameraFeed, closeCameraFeed, stopGstLoopbackView, startSpotCamerasImageServiceView, stopSpotCamerasImageServiceView
 
 
 # Main
@@ -15,8 +15,6 @@ from .views import ApiRoutes, HelloSpot, getCameraFeed, closeCameraFeed, startGs
 * /api/hello-spot/              ->      Execute HelloSpot
 * /api/camera/                  ->      Get live camera feed
 * /api/close-camera             ->      Close live camera feed
-* /api/start-gst-loopback       ->      Execute gst_loopback
-* /api/stop-gst-loopback        ->      Stop gst_loopback
 * /api/start-spot-camera        ->      Run SpotCameras image service
 * /api/stop-spot-camera         ->      Stop SpotCameras image service
 """
@@ -25,8 +23,6 @@ urlpatterns = [
     path('hello-spot/', HelloSpot.as_view(), name='api-hello-spot'),
     path('camera/', getCameraFeed, name='api-get-camera-feed'),
     path('close-camera/', closeCameraFeed, name='api-close-camera'),
-    path('start-gst-loopback/', startGstLoopbackView, name='api-start-gst-loopback'),
-    path('stop-gst-loopback/', stopGstLoopbackView, name='api-stop-gst-loopback'),
     path('start-spot-cameras/', startSpotCamerasImageServiceView, name='api-start-spot-cameras'),
     path('stop-spot-cameras/', stopSpotCamerasImageServiceView, name='api-stop-spot-cameras'),
 ]
